45edfd7e549f247f14dd30f82db99103bb143eef
Shahinur Kabir <shahinurkabir@yahoo.com>
2020-09-19 13:46:14 +0600

    Added class PersonViewModel.cs and new propery added EmailAddress in Person.cs file

--- Person.cs
+++ Person.cs
@@ -3,4 +3,5 @@ public class Person
     public  int Id { get; set; }     
     public string Name { get; set; }
     public string Address   {get;set;}
+    public string EmailAddress { get; set; }    
 }
---
+++ PersonViewModel.cs
@@ -0,0 +1,7 @@
+public class PersonViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; } 
+    public string EmailAddress { get; set; }    
+}
